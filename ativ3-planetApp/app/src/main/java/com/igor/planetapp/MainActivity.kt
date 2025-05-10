89292515062ac9762fc624761c03f7801b8a3c5f
igor albuquerque <igoralbuquerque102@hotmail.com>
2025-05-10 17:07:30 -0300

    Atividade 3 planetApp feita

---
+++ ativ3-planetApp/app/src/main/java/com/igor/planetapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.igor.planetapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.igor.planetapp.navigation.NavGraph
+import com.igor.planetapp.ui.theme.PlanetAppTheme
+
+@ExperimentalMaterial3Api
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavGraph(
+                onSettingsClick = {
+                },
+                onHelpClick = {
+                }
+            )
+        }
+    }
+}
---
+++ ativ3-planetApp/app/src/main/java/com/igor/planetapp/ui/screens/FavoritesScreen.kt
@@ -0,0 +1,68 @@
+package com.igor.planetapp.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.*
+import com.igor.planetapp.models.Planet
+import com.igor.planetapp.models.planetList
+import com.igor.planetapp.ui.components.PlanetListItem
+
+@ExperimentalMaterial3Api
+@Composable
+fun FavoritesScreen(
+    onPlanetSelected: (Planet) -> Unit,
+    onFavoriteToggle: (Planet) -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Favoritos",
+
+                        style = MaterialTheme.typography.titleLarge
+
+                    )
+                }
+            )
+        }
+    ) { innerPadding ->
+        val favoritePlanets = planetList.filter { it.isFavorite }
+        if (favoritePlanets.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Você ainda não adicionou favoritos.",
+                    style = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        } else {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .padding(horizontal = 8.dp)
+            ) {
+                items(favoritePlanets) { planet ->
+                    PlanetListItem(
+                        planet = planet,
+
+                        onPlanetSelected = { onPlanetSelected(it) },
+                        onFavoriteToggle = { onFavoriteToggle(it) }
+                    )
+                }
+            }
+        }
+    }
+}
