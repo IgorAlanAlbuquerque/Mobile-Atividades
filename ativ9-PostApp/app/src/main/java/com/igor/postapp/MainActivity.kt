34f073e0af96e73bffc31f2758990848ad91d6b6
igor albuquerque <igoralbuquerque102@hotmail.com>
2025-06-22 19:07:22 -0300

    atividade 9 "concluida"

---
+++ ativ9-PostApp/app/src/main/java/com/igor/postapp/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.igor.postapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.igor.postapp.ui.screens.PostScreen
+import com.igor.postapp.ui.screens.UserScreen
+import com.igor.postapp.ui.theme.PostAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState )
+        enableEdgeToEdge()
+        setContent {
+            PostAppTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api ::class)
+@SuppressLint ("UnusedMaterial3ScaffoldPaddingParameter" )
+@Composable
+fun MainScreen () {
+    var selectedTab by remember { mutableIntStateOf(0) }
+    Scaffold (
+        topBar = {
+            TopAppBar (
+                title = { Text("AppDev" ) },
+                colors =
+                    TopAppBarDefaults.centerAlignedTopAppBarColors (
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = MaterialTheme.colorScheme.onPrimary
+                    )
+            )
+        },
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    selected = selectedTab == 0,
+                    onClick = { selectedTab = 0 },
+                    label = { Text("Usuários") },
+                    icon = { Icon(Icons.Default.Person, contentDescription =
+                        "Usuários") }
+                )
+                NavigationBarItem(
+                    selected = selectedTab == 1,
+                    onClick = { selectedTab = 1 },
+                    label = { Text("Posts") },
+                    icon = { Icon(Icons.AutoMirrored.Filled.List, contentDescription = "Posts")
+                    }
+                )
+            }
+        }
+    ) {
+        when (selectedTab) {
+            0 -> UserScreen()
+            1 -> PostScreen()
+        }
+    }
+}
